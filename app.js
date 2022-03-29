18694ce9dd9b8bdf828f45b3c3c6a1e5fd954ed9
Richard Ogujawa <richardogujawa7@gmail.com>
2022-03-29 23:03:44 +0100

    started using JQuery

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".fa-bars").click(function() {
+        $(".scroll-ul").addClass("scroll-active"); 
+    });
+
+    //https://www.youtube.com/watch?v=BfH5aeeZvh0 WATCH THIS TO KNOW HOW TO DO THE HAMBURGER ICON
+});
